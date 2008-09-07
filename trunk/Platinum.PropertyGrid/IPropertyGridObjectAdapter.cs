0a0cf1690d5935f3e2106aa4e52e2e43818bf930
michaelwoerister <michaelwoerister@233be3c4-e955-0410-b229-c70aa0688464>
2008-09-07 16:17:27 +0000

    - Added missing file IPropertyGridObjectAdapter.cs - Removed DefaultPropertyGridFiller.cs from the c# project

---
+++ trunk/Platinum.PropertyGrid/IPropertyGridObjectAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platinum
+{
+    public interface IPropertyGridObjectAdapter
+    {
+        Object SelectedObject
+        {
+            get;
+            set;
+        }
+
+        PropertyGrid TargetPropertyGrid
+        {
+            get;
+            set;
+        }
+    }
+}
